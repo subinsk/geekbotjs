15cc2278d2d6c5772d504a4f821d0f1c1a218e57
Subin <subinsk284@gmail.com>
2021-08-07 17:24:44 +0530

    main

--- commands/codeforces.js
+++ commands/codeforces.js
@@ -29,39 +29,16 @@ async function main() {
     let data = JSON.parse(uc);
     upcoming_contests = data['result'].filter((con) => {
         return con.phase = 'BEFORE';
-    }).slice(1, 15);
+    }).slice(1, 10);
     upcoming_contests.forEach((obj) => names.push(String(obj.name)))
     upcoming_contests.forEach((obj) => {
         var date = new Date(Number(obj.startTimeSeconds));
-        date.toString("hh mm ss dd MM yyyy");
+        date.toString("dd MM");
         dates.push(String(date))
     })
     return { names, dates };
 
 }
-
-let contest= main();
-
-// async function getNames() {
-//     let { names } = await main();
-//     return names;
-    // async function getNames(){
-    //     return names;
-    // }
-    // async function getDates(){
-    //     return dates;
-    // }
-// }
-
-
-// names.forEach((name)=> { return String(name)})
-
-// console.log(String(names.forEach((name)=>{return `${name}\n`})))
-// Object.keys(obj)
-//     .forEach(key=>{
-//             obj[key]
-//         })
-//     );
 
 module.exports = {
     name: 'codeforces',
@@ -69,6 +46,23 @@ module.exports = {
     async execute(bot, message, args, Discord) {
         let contestsEmbed = await new Discord.MessageEmbed()
         async function getEmbed() {
+            let cont=await main();
+            let contFields=[];
+            for(let i=0;i<cont.names.length;i++){
+                contFields.push(
+                    {
+                        name: cont.names[i],
+                        value: cont.dates[i],
+                        inline: false
+
+                    },
+                    {
+                        name: '\u200b',
+                        value: '\u200b',
+                        inline: false,
+                },
+                )
+            }
             return {
                 color: 0x0099ff,
                 title: 'Codeforces Upcoming Contests',
@@ -81,33 +75,21 @@ module.exports = {
                 thumbnail: {
                     url: 'https://assets.codeforces.com/images/codeforces-sponsored-by-vk-en.PNG',
                 },
-                fields: [
-                    {
+                field:{
+                    
                         name: '\u200b',
                         value: '\u200b',
                         inline: false,
-                    },
-                    {
-                        name: 'Contest Name',
-                        value: `${contest.names.forEach((name)=>{return name})}`,
-                        inline: true,
-                    },
-                    {
-                        name: 'Contest Date',
-                        value: `${contest.dates}`,
-                        inline: true,
-                    },
-                    {
-                        name: '\u200b',
-                        value: '\u200b',
-                        inline: false,
-                    },
-                    {
-                        name: '\u200b',
-                        value: '\u200b',
-                        inline: false,
-                    },
-                ],
+                    
+                },
+                fields: contFields,
+                field:{
+                    
+                    name: '\u200b',
+                    value: '\u200b',
+                    inline: false,
+                
+            },
                 image: {
                     url: 'https://assets.codeforces.com/images/codeforces-sponsored-by-vk-en.PNG',
                 },
@@ -123,15 +105,3 @@ module.exports = {
         message.channel.send({ embed: contestsEmbed });
     }
 }
-
-//     const contestsEmbed = await new Discord.MessageEmbed()
-//         .setColor('#0099ff')
-    //         .setTitle('Codeforces Upcoming Contests')
-    //         .setThumbnail('https://assets.codeforces.com/images/codeforces-sponsored-by-vk-en.PNG')
-    //         .addField('Name of Contest', getContestNames(), true)
-    //         // .addField('Start time', dates.forEach(), true)
-    //         .setImage('https://assets.codeforces.com/images/codeforces-sponsored-by-vk-en.PNG')
-    //         .setTimestamp()
-    //     channel.send({ embeds: [contestsEmbed] });
-    // }
-// }
---
+++ commands/help.js
@@ -0,0 +1,57 @@
+module.exports={
+    name:'help',
+    description:'Help',
+    async execute(bot,message,args, Discord ){
+        let helpEmbed = await new Discord.MessageEmbed()
+
+        async function getEmbed(){
+            return {
+                color: 0x0099ff,
+                title: 'Hey Developers!\n',
+                description:'GeekBOT Here :)\n \n \n',
+
+                author: {
+                    name: 'Subin',
+                    icon_url: 'https://cdn.discordapp.com/avatars/802070431938641964/175ab0128e7203adbe61e846a4e42500.png?size=256',
+                    url: 'https://www.linkedin.com/in/subin-s-k-9b767219a/',
+                },
+
+                field: {
+                    name: '\n',
+                    value: '\n',
+                    inline: false,
+                },
+
+                fields: [
+                    {
+                        name:'Commands:\n',
+                        value:
+                            '>codeforces - Latest Codeforces Contest Details\n\n'
+                           +'>technews - Latest Tech related News\n\n'
+                           +'>avatar *@taggedUser* - To get URL for your Discord Avatar \n\n'
+                           +'>rank *@taggedUser* - To get your present rank \n\n'
+                           +'>image *imageQuery* - To get an image related to the query \n\n'
+                           +'>ping  - Something interesting ;) \n\n'
+                           +'>help  - For help related to Bot and Commands ;) \n\n'
+                    }
+                ],
+
+                field: {
+                    name: '\n ',
+                    value: '\n ',
+                    inline: false,
+                },
+
+                timestamp: new Date(),
+
+                footer: {
+                    text: 'Developed By Subin',
+                    icon_url: 'https://cdn.discordapp.com/avatars/802070431938641964/175ab0128e7203adbe61e846a4e42500.png?size=256',
+                }
+            }
+        } 
+
+        helpEmbed = await getEmbed();
+        message.channel.send({embed: helpEmbed});
+    }
+}
