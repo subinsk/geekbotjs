2b55fbc0a94603c3bb182a9b2664e5949fe272f7
Subin S K <76201673+subinsk@users.noreply.github.com>
2021-08-20 13:14:43 +0000

    changed automated messages

--- commands/ping.js
+++ commands/ping.js
@@ -2,6 +2,6 @@ module.exports ={
   name:'ping',
   description: 'Ping message',
   async execute(bot, message, args,Discord){
-    message.reply("\n Ping Pong \n Ding Ding Ding \n Me Ninja Hattori \n aa gaya hu \n Main Ninja Hattori..... Aaa Gaya Hoon!")
+    message.reply("\n Hey!")
   }
 }
