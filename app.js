5d4d648cf25a433c83e83a5091a8c7c067327b94
Subin <subinsk284@gmail.com>
2021-08-23 21:37:42 +0530

    app js updated according to vercel

--- app.js
+++ app.js
@@ -12,6 +12,16 @@ const bot = new Discord.Client();
 bot.login(process.env.DISCORD_TOKEN)
 
 
+if (
+  process.env.LD_LIBRARY_PATH == null ||
+  !process.env.LD_LIBRARY_PATH.includes(
+    `${process.env.PWD}/node_modules/canvas/build/Release:`,
+  )
+) {
+  process.env.LD_LIBRARY_PATH = `${
+    process.env.PWD
+  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
+}
 
 // Loading bot commands
 bot.commands = new Discord.Collection();
